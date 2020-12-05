b8fdcb348df23b3d4c5c067e0dbfe7f9ac400b58
stevechacha <stevechacha4@gmail.com>
2020-12-05 10:35:34 +0300

    second commit

---
+++ app/src/main/java/com/example/firebaseandroid/data/remote/MusicDatabase.kt
@@ -0,0 +1,5 @@
+package com.example.firebaseandroid.data.remote
+
+class MusicDatabase {
+
+}
---
+++ app/src/main/java/com/example/firebaseandroid/entities/Songs.kt
@@ -0,0 +1,9 @@
+package com.example.firebaseandroid.entities
+
+data class Songs(
+        val mediaId:String="",
+        val title : String="",
+        val subtitle:String="",
+        val songUrl:String="",
+        val imageUrl:String=""
+)
